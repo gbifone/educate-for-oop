fbfb8fdecf58dbcdf79bc2be3c1bfe62f88d2f6b
kinza <kinza.fatima.522@gmail.com>
2017-05-23 12:23:55 -0700

    Assignment_Added_23_05_2017

---
+++ java/book-dietel/kinza/HackerRankDay_0.java
@@ -0,0 +1,17 @@
+/**
+ * Created by kinza mustafa on 5/23/2017.
+ */
+import java.util.Scanner;
+public class HackerRankDay_0 {
+    public static void main ( String[] args ){
+        Scanner in = new Scanner ( System.in );
+
+        String input_string;
+        System.out.println ( "Enter a string");
+        input_string = in.nextLine();
+        System.out.println ( "Hello, World.");
+        System.out.println ( input_string );
+
+
+    }
+}
