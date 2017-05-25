15429b8957ea4b4948cfbb5f1026ea97a21ecf55
madeehaafzal <afzalmadiha2@gmail.com>
2017-05-25 09:34:43 +0500

    Madiha_Assignment_Added_24_05_2017

---
+++ java/book-dietel/madiha/Example7_19.java
@@ -0,0 +1,22 @@
+/**
+ * Created by TOSHIBA on 5/24/2017.
+ */
+public class Example7_19 {
+        public static void main ( String[] args )
+        {
+            int[][] gradesArray = {{87, 96, 70},
+                    {68, 87, 90},
+                    {94, 100, 90},
+                    {100, 81, 82},
+                    {83, 65, 85},
+                    {78, 87, 65},
+                    {85, 75, 83},
+                    {91, 94, 100},
+                    {76, 72, 84},
+                    {87, 93, 73}};
+            Example7_18 myGradeBook = new Example7_18 ( "CS101 Introduction to Java Programming", gradesArray);
+            myGradeBook.display();
+            myGradeBook.processGrade();
+        }
+    }
+
