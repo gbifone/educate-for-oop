7ecf5e4bb270996f471a0eca5925f2ce65492db0
Kinza <kinza.fatima.522@gmail.com>
2017-05-27 11:04:55 -0700

    Evaluation_27_05_2017

---
+++ java/book-dietel/kinza/HospitalRecord.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Kinza Fatima on 5/27/2017.
+ */
+public class HospitalRecord {
+    private String disease;
+
+    public String getDisease ( ) {
+        return disease;
+    }
+
+    public void setDisease ( String disease ) {
+        if ( disease == null ){
+            disease = "null";
+        } else {
+            this.disease = disease;
+        }
+
+    }
+    public String[] searchPatientswithDisease( String disease){
+        for(int i = 0; i <){
+            if( ){
+
+            }
+        }
+
+    }
+}
