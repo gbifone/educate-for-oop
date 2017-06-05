7fe5e95c2acfaf67ed5314f61221f2fc1def459a
Nadia Gull <nadiagull285@gmail.com>
2017-06-05 08:43:23 +0500

    nadia_assignment_chapter8_4/6/2017

--- java/book-dietel/nadia/Date.java
+++ java/book-dietel/nadia/Date.java
@@ -4,57 +4,44 @@ public class Date {
        private int month;
        private String sMonth;
        private int year;
-
-
     public Date(int day, int month, int year) {
         this.day = day;
         this.month = month;
         this.year = year;
     }
-
     public Date(int day, String sMonth ,int year) {
         this.day = day;
         this.sMonth = sMonth;
         this.year = year;
     }
-
     public Date(int day, int year) {
         this.day = day;
         this.year = year;
     }
-
     public int getDay() {
         return day;
     }
-
     public void setDay(int day) {
         this.day = day;
     }
-
     public int getMonth() {
         return month;
     }
-
     public void setMonth(int month) {
         this.month = month;
     }
-
     public String getsMonth() {
         return sMonth;
     }
-
     public void setsMonth(String sMonth) {
         this.sMonth = sMonth;
     }
-
     public int getYear() {
         return year;
     }
-
     public void setYear(int year) {
         this.year = year;
     }
-
     public void displayDate1(){
         System.out.println(this.month + "/" + this.day + "/" + this.year);
     }
