0f45a273c2f591cf510d6070e101a7a5855eec0b
Nadia Gull <nadiagull285@gmail.com>
2017-06-05 08:33:36 +0500

    nadia_assignment_chapter8_4/6/2017

---
+++ java/book-dietel/nadia/DateTest.java
@@ -0,0 +1,11 @@
+
+public class DateTest {
+    public static void main(String[] args) {
+         Date d1 = new Date(4,6,2017);
+         Date d2 = new Date(4,"june",2017);
+         Date d3 = new Date(321,2017);
+         d1.displayDate1();
+         d2.displayDate2();
+         d3.displayDate3();
+    }
+}
